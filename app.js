f7a5a23c22c53d38055c7ec4e15472d73c611fc8
Christopher McCloud <mccloud.christopher@gmail.com>
2014-09-02 13:50:45 -0700

    (feat) adds basic $http router service

--- app.js
+++ app.js
@@ -1,46 +1,20 @@
 /**
 	* Created by Macnube on 8/29/14.
 	*/
-angular.module('myApp', [])
-.controller('formController', function($scope) {
+var app = angular.module('myApp', []);
+
+app.controller('formController', function($scope, $q, RouterService, $http) {
 				$scope.myForm = {};
 				$scope.myForm.name = "";
 				$scope.myForm.email = "";
 				$scope.myForm.value = "";
 				$scope.clickSubmit = function() {
 						if ($scope.myForm.name && $scope.myForm.email && $scope.myForm.value) {
-								//callPost($scope.form.name, $scope.form.email, $scope.form.value);
-								console.log('Click Submit Fired!');
-						}
-						else { console.log('Need Inputs!')}
+								console.log('Click Submit Fired! in formController - app.js');
+								RouterService.formPostTest($scope.myForm);
+						} else { console.log('Need Inputs!')}
 				}
 		});
 
-angular.module('d3', [])
-.factory('d3Service', ['$document', '$q', '$rootScope',
 
-				function($document, $q, $rootScope) {
-						var d = $q.defer();
-
-						function onScriptLoad() {
-								$rootScope.$apply(function() {d.resolve(window.d3); });
-						}
-
-						var scriptTag = $document[0].createElement('script');
-						scriptTag.type ='text/javascript';
-						scriptTag.async = true;
-						scriptTag.src = 'http://d3js.org/d3.v3.min.js';
-						scriptTag.onreadystatechange = function () {
-								if (this.readyState == 'complete') onScriptLoad();
-						};
-						scriptTag.onload = onScriptLoad;
-
-						var body = $document[0].getElementsByTagName('body')[0];
-						body.appendChild(scriptTag);
-
-				return {
-						d3 : function() { return	d.promise; }
-				};
-
-		}]);
 
---
+++ router.js
@@ -0,0 +1,50 @@
+app
+		.service('RouterService', function($http, $q) {
+
+				//API
+				return({
+						formPost: formPost,
+						formPostTest : formPostTest
+				});
+
+				//public methods
+				function formPost(formObj) {
+						var name = formObj.name;
+						var email = formObj.email;
+						var value = formObj.value;
+
+						var request = $http({
+								method: 'POST',
+								url: 'serverURLHERE',
+								data: {
+										name : name,
+										email : email,
+										value : value
+								}
+						});
+
+						return (request.then(handleSuccess, handleError));
+
+				}
+				function formPostTest (formObj) {
+						return console.log(formObj);
+				}
+
+				//private methods
+
+				function handleError(response) {
+						if (
+								! angular.isObject(response.data) ||
+								! response.data.message
+								) { return	$.reject('An unknown error occurred.');
+						} else {
+								return $q.reject(response.data.message);
+						}
+				}
+
+				function handleSuccess(response) {
+						return response.data;
+				}
+		});/**
+	* Created by Macnube on 9/2/14.
+	*/
