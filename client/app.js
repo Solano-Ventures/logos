469348fb36614fc0dcb380bc8774aa9f5e211d2c
Christopher McCloud <mccloud.christopher@gmail.com>
2014-09-03 10:59:27 -0700

    (cleanup) trimmed whitespaces per styleguide

--- client/app.js
+++ client/app.js
@@ -1,23 +1,20 @@
 angular.module('myApp', ['ngRoute'])
-    .config(function($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/formView.html',
-                controller: 'FormController'
-            });
-    }).
-    controller('FormController', function($scope, $q, RouterService) {
-            $scope.myForm = {};
-            $scope.myForm.name = "";
-            $scope.myForm.email = "";
-            $scope.myForm.value = "";
-            $scope.clickSubmit = function() {
-                if ($scope.myForm.name && $scope.myForm.email && $scope.myForm.value) {
-                    console.log('Click Submit Fired! in formController - app.js');
-                    RouterService.formPostTest($scope.myForm);
-                } else { console.log('Need Inputs!')}
-            }
-        });
-
-
-
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/formView.html',
+        controller: 'FormController'
+      });
+  })
+  .controller('FormController', function ($scope, $q, RouterService) {
+    $scope.myForm = {};
+    $scope.myForm.name = "";
+    $scope.myForm.email = "";
+    $scope.myForm.value = "";
+    $scope.clickSubmit = function () {
+      if ($scope.myForm.name && $scope.myForm.email && $scope.myForm.value) {
+        console.log('Click Submit Fired! in formController - app.js');
+        RouterService.formPostTest($scope.myForm);
+      } else { console.log('Need Inputs!'); }
+    };
+  });
