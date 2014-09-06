7fab6f33b336fe289238db29b2a1dfd4b38da833
singalongwithme <csinghavong@gmail.com>
2014-09-06 12:27:36 -0700

    (feat) implement basic server

--- client/controllers/signupController.js
+++ client/controllers/signupController.js
@@ -1,6 +1,5 @@
 angular.module('myApp.controllers')
   .controller('SignupController', function ($scope, $q, RouterService) {
-<<<<<<< HEAD
     $scope.data = {};
     $scope.data.email = "";
     $scope.data.password = "";
@@ -8,15 +7,6 @@ angular.module('myApp.controllers')
       console.log('Click Submit Fired!');
       if($scope.data.email && $scope.data.password){
         RouterService.postSignUp($scope.data);
-=======
-    $scope.signupForm = {};
-    $scope.signupForm.email = "";
-    $scope.signupForm.password = "";
-    $scope.signupSubmit = function(){
-      if($scope.signupForm.email && $scope.signupForm.password){
-        console.log("Signup Submit was fired!");
-        RouterService.authPost($scope.signupForm);
->>>>>>> (feat) built basic mongo server
       } else {
         console.log("needs all inputs");
       }
