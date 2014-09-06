a8c488b2da41e2539a89efcf5d77b6c8a444eced
Jon Sadka <jonsadka@gmail.com>
2014-09-06 14:46:29 -0700

    (fix) logo now generates with selected color

--- client/controllers/landingController.js
+++ client/controllers/landingController.js
@@ -4,9 +4,20 @@ angular.module('myApp.controllers')
     $scope.data.firstName = "";
     $scope.data.lastName = "";
     $scope.data.definedBy = "";
-    //$scope.data.color = "";
+    $scope.data.color = "";
+
+    $scope.hexToRgb = function(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        console.log(hex, result)
+        return result ? 'RGBA(' + parseInt(result[1], 16) + ',' + 
+                                  parseInt(result[2], 16) + ',' +
+                                  parseInt(result[3], 16) + ',1.0)' : null;
+    };
+
     $scope.clickSubmit = function () {
       if ($scope.data.firstName && $scope.data.lastName && $scope.data.definedBy ) {
+        $scope.data.color = $scope.hexToRgb($scope.data.color);
+        console.log($scope.data.color)
         RouterService.formPost($scope.data);
       } else { console.log('Need Inputs!'); }
     };
